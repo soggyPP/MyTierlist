951a802af588fc4c342228ec24a48f060b890d76
soggyPP <148140354+soggyPP@users.noreply.github.com>
2023-10-26 02:51:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var num=10;
-document.addEventListener("click", function a() {
-  while(num>=1) {
-  alert(num + "");
-  num--;
+function a() {
+  var x = (`nigga it ain't built yet,
+countdown to your death starts now.`)
+var i = 10 
+alert(x)
+function inner() {
+while(i>=1) {
+  alert(i--)
+  if(i==1) {
+    document.getElementById("doom.html")
+  }
+  }
 }
-});
+setTimeout(function inner(){while(i>=1) {
+  alert(i--)
+  } }, 1000)
+}
+
